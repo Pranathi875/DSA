706c248457067057dbe6ae27f3d9ea35e8c376c8
Pranathi Bendalam Vakeelu <pranathibendalamv293@gmail.com>
2026-09-15 19:15:56 +0530

    Implement XOR method for missing number problem
    Added a more optimal solution using XOR to find the missing number.

--- arrays/leetcode_268_optimal.java
+++ arrays/leetcode_268_optimal.java
@@ -1,3 +1,4 @@
+##1--optimal
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
@@ -7,5 +8,20 @@ class Solution {
             sum+=nums[i];
         }
         return totalSum-(int)sum;
+    }
+}
+##2--more optimal using xor
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1=0;
+        int xor2=0;
+       for(int i=0;i<=nums.length;i++){
+            xor1^=i;
+       }
+       for(int i=0;i<nums.length;i++){
+            xor2^=nums[i];
+       }
+       return xor1^xor2;
     }
 }
